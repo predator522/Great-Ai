0a933ca75f1fec7922f293a06b31ad87e4094eab
predator522 <p47017988@gmail.com>
2025-09-15 12:25:26 +0000

    Create config.js

---
+++ config/config.js
@@ -0,0 +1,7 @@
+const API_OPTIONS = {
+  greatai: {
+    endpoint: "https://openrouter.ai/api/v1/chat/completions",
+    key: "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",
+    model: "openai/gpt-3.5-turbo"
+  }
+};
